d5494d0a6e400b2a5e2a89b90f1f8f3079e65e4c
Шелохин Игорь <uropb2013@yandex.ru>
2021-12-01 18:46:52 +0300

    days_between complete

---
+++ days_between.py
@@ -0,0 +1,15 @@
+def days_diff(a, b):
+    # your code here
+    from datetime import datetime
+    return abs(datetime(a[0], a[1], a[2]) - datetime(b[0], b[1], b[2])).days
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(days_diff((1982, 4, 19), (1982, 4, 22)))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+    print("Coding complete? Click 'Check' to earn cool rewards!")
