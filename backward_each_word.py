c5bec75501ea6f4ec727a85a75720127038eca10
Шелохин Игорь <uropb2013@yandex.ru>
2021-12-02 19:01:47 +0300

    backward_each_word complete

---
+++ backward_each_word.py
@@ -0,0 +1,15 @@
+def backward_string_by_word(text: str):
+    return ' '.join([x[::-1] for x in text.split(' ')])
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(backward_string_by_word('welcome to a game'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert backward_string_by_word('') == ''
+    assert backward_string_by_word('world') == 'dlrow'
+    assert backward_string_by_word('hello world') == 'olleh dlrow'
+    assert backward_string_by_word('hello   world') == 'olleh   dlrow'
+    assert backward_string_by_word('welcome to a game') == 'emoclew ot a emag'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
